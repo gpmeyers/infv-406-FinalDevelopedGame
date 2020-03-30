4fbc583af4f83db77b357f460189704e4e57a99f
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-03-29 23:56:07 -0700

    Added Player Movement; Need to work on jump
    Jump makes the player fly, I need to make it so it only applies the upward force if it is a frame that the player is on the ground

---
+++ game-demo/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 1000f;
+    public float sidewardForce = 600f;
+    public float jumpForce = 400f;
+
+    // We are using Fixed Update because we are using this for player physics.
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // TODO: Make this so that the jump key feels more like a jump and not flying
+        // Player movement Jump from user input "w"
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+        }
+
+        // Player Movement Right from user input "d"
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewardForce * Time.deltaTime, 0, 0);
+        }
+
+        // Player Movement Left from user input "a"
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewardForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
