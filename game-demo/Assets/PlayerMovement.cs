6186dd7dcab8581d80cb6afdd22bea5563292155
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-03-30 23:53:24 -0700

    Decided to Remove Jumping ability from player Movement
    I want to make this game an endless runner but decided against the 3-lane endless runner with changing lanes and jumping and wanted to make one with just sideward movement

--- game-demo/Assets/PlayerMovement.cs
+++ game-demo/Assets/PlayerMovement.cs
@@ -1,38 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public Transform player;
-
     public float forwardForce = 1000f;
     public float sidewardForce = 600f;
-    public float jumpForce = 2000f;
-    private bool onGround = true;
 
     // We are using Fixed Update because we are using this for player physics.
     void FixedUpdate()
     {
-        if(player.position.y <= 1.1 && player.position.x >= -8.0 && player.position.x <= 8.0)
-        {
-            onGround = true;
-        }
-        else
-        {
-            onGround = false;
-        }
-
         // Add a forward force
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        // Player movement Jump from user input "w"
-        if (Input.GetKey("w") && onGround == true)
-        {
-            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
-        }
 
         // Player Movement Right from user input "d"
         if (Input.GetKey("d"))
