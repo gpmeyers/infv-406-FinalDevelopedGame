fdc10a30271588edbaa3e19a4bce1915ccef4029
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-03-30 20:16:55 -0700

    Player Movement Finished
    Player Movement is finished, I will have to mess with the specific force number to make it feel fluid and make some more design decieions in the future I also have to add camera follow

--- game-demo/Assets/PlayerMovement.cs
+++ game-demo/Assets/PlayerMovement.cs
@@ -6,19 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public Transform player;
+
     public float forwardForce = 1000f;
     public float sidewardForce = 600f;
-    public float jumpForce = 400f;
+    public float jumpForce = 2000f;
+    private bool onGround = true;
 
     // We are using Fixed Update because we are using this for player physics.
     void FixedUpdate()
     {
+        if(player.position.y <= 1.1 && player.position.x >= -8.0 && player.position.x <= 8.0)
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+
         // Add a forward force
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        // TODO: Make this so that the jump key feels more like a jump and not flying
         // Player movement Jump from user input "w"
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") && onGround == true)
         {
             rb.AddForce(0, jumpForce * Time.deltaTime, 0);
         }
