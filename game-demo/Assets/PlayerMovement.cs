6a1269a122419cbc83d082c0c1cdc353027187c7
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-03-31 14:01:51 -0700

    Added back jump mechanic
    Found a way to make the jump mechanic only work when the player is on the ground and also not have horizontal momentum

--- game-demo/Assets/PlayerMovement.cs
+++ game-demo/Assets/PlayerMovement.cs
@@ -4,25 +4,46 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float forwardForce = 1000f;
-    public float sidewardForce = 600f;
+    private Vector3 moveVector;
 
-    // We are using Fixed Update because we are using this for player physics.
+    public float speed = 5f;
+
+    public float jumpForce = 1000f;
+
+    private bool onGround = true;
+
+    // This will be called once per frame. Using FixedUpdate because some physics will be declared here.
     void FixedUpdate()
     {
-        // Add a forward force
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        // Player Movement Right from user input "d"
-        if (Input.GetKey("d"))
+        // Check for if our Rigid Body is on the ground or not
+        if(rb.position.y <= 1.1 && rb.position.x <= 8 && rb.position.x >= -8)
         {
-            rb.AddForce(sidewardForce * Time.deltaTime, 0, 0);
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
         }
 
-        // Player Movement Left from user input "a"
-        if (Input.GetKey("a"))
+        // Jump mechanic
+        if((Input.GetKey("w") || Input.GetKey("up")) && onGround)
         {
-            rb.AddForce(-sidewardForce * Time.deltaTime, 0, 0);
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
         }
+
+        // Reset the movement vector for a new update
+        moveVector = Vector3.zero;
+
+        // Player can not move horizontally while jumping
+        if (onGround)
+        {
+            moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        }
+
+        // Player will always receive a constant forward speed whether in the air or on the ground
+        moveVector.z = speed;
+
+        // Update the position of the RigidBody (our player character)
+        rb.position = rb.position + (moveVector * Time.deltaTime);
     }
 }
