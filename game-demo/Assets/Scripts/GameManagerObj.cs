c95cd7af5e19def8a842ef2acaf49571cd5bdbf0
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-04-08 01:15:57 -0700

    Added EndGame state and level restart

---
+++ game-demo/Assets/Scripts/GameManagerObj.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerObj : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    // Function for the Game Manager to perform tasks for endgame state
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            FindObjectOfType<Score>().gameOver = true; // Stop the scoreboard from increasing to show the player's final score
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    // Function to restart the game
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- game-demo/Assets/Scripts/PlayerCollision.cs
+++ game-demo/Assets/Scripts/PlayerCollision.cs
@@ -4,13 +4,18 @@ public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
 
-    // Function will run anytime our player collides with another rigidbody
+    /*
+     *Function will run anytime our player collides with another rigidbody
+     * 
+     * collisionInfo is a reference to the collision that we can obtain information from
+     */
     void OnCollisionEnter(Collision collisionInfo)
     {
         if(collisionInfo.collider.tag == "Obstacle")
         {
-            movement.rb.constraints = RigidbodyConstraints.None;
-            movement.enabled = false;
+            movement.rb.constraints = RigidbodyConstraints.None; // Allow rotation for physics effects
+            movement.enabled = false; // Disable Player movement
+            FindObjectOfType<GameManagerObj>().EndGame();
         }
     }
 }
