bccf46c9ebeac5f4d272229e91fad8c5292b3325
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-04-08 00:44:11 -0700

    Added Scoreboard UI

---
+++ game-demo/Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+
+    public bool gameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            scoreText.text = player.position.z.ToString("0");
+        }
+    }
+}
