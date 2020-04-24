a52bacbbd51136accf1e4ce96559a6ba76eb78cc
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-04-23 19:37:10 -0500

    Platform Generation Script Working for Finite Platforms
    The Platform Generation Script has been created and works for a Finite number of platforms, need to fix it to work on an infinite number of platforms

---
+++ game-demo/Assets/GenerateWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    public GameObject[] platforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = new Vector3(0, 0, 250);
+        for(int i = 0; i < 20; i++)
+        {
+            int platformNumber = Random.Range(0, platforms.Length);
+            Instantiate(platforms[platformNumber], pos, Quaternion.identity);
+            pos.z += 250;
+        }
+    }
+}
