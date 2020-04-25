40eb50c5932e262d55cd192fca93642c8b3ebd83
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-04-25 16:27:34 -0500

    UI and Main Menu Complete
    The Main Menu, Settings Menu, and all other UI elements are complete

---
+++ game-demo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    // This function will be called when the user selects the play button at the main menu and will begin the game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // This function will be called when the user selects the quit button at the main menu and will end the application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
